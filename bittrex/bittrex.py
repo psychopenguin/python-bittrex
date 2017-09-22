d39c5486ed03b1274898dd1c7bfe32aa45fd16ce
Fabio Brito <psychopenguin@gmail.com>
2017-09-23 00:18:09 +0800

    change init method to allow object initialization without parameters creating a public access object

--- bittrex/bittrex.py
+++ bittrex/bittrex.py
@@ -75,7 +75,7 @@ class Bittrex(object):
     """
     Used for requesting Bittrex with API key and API secret
     """
-    def __init__(self, api_key, api_secret, dispatch=using_requests):
+    def __init__(self, api_key=None, api_secret=None, dispatch=using_requests):
         self.api_key = str(api_key) if api_key is not None else ''
         self.api_secret = str(api_secret) if api_secret is not None else ''
         self.dispatch = dispatch
