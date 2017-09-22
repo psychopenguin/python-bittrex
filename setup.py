8d8581b881add81fb87921cff023ffc42ab0702c
Fabio Brito <psychopenguin@gmail.com>
2017-09-23 00:19:20 +0800

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 
 setup(name='python-bittrex',
-      version='0.1.3',
+      version='0.1.4',
       packages=['bittrex'],
       modules=['bittrex'],
       install_requires=['requests'],
